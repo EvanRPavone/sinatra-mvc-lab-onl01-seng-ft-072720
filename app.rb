b7864c76096a56b7c3a25d5a30e7ec6b0dfa9004
Evan Pavone <evanrpavone@gmail.com>
2020-09-07 16:30:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
+  get '/' do
     erb :user_input
   end
 
-  post '/piglatinize' do 
+  post '/piglatinize' do
     phrase = PigLatinizer.new
     @user_input = phrase.to_pig_latin(params[:user_phrase])
     erb :piglatinize
